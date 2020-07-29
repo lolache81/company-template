7869eefe725d37b4d069d10e378c9cb88eb7e2e1
lolache81 <68953574+lolache81@users.noreply.github.com>
2020-07-29 21:38:39 +0800

    workshop

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,20 +5,17 @@
 
       PARAMETERS
       :logoURL  (URL for Logo)
-     -->
-    <NaviWrapper
-      imgURL="/img/lead-white-logo.png"
-      ></NaviWrapper>
+    -->
+    <NaviWrapper imgURL="https://f0.pngfuel.com/png/196/518/coffee-house-logo-png-clip-art.png"></NaviWrapper>
 
-    <div class='flex-1'>
-      <div class='flex'>
-        <Nuxt class="w-full h-screen overflow-y-auto" />
+    <div class="flex-1">
+      <div class="flex">
+        <Nuxt class="w-full h-screen overflow-y-auto"/>
       </div>
     </div>
-    <FooterWrapper />
+    <FooterWrapper/>
   </div>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,9 @@
           :second_header ( Header 2 Text)
           :descriptions ( Descriptions )
         -->
-        <HeaderContent 
-          imgURL="/img/slider1.jpeg"
-          header_txt="Company Template by LEAD"
+        <HeaderContent
+          imgURL="https://images.indianexpress.com/2019/07/filter-coffee_759.jpg"
+          header_txt="Testing 123"
           second_header="Second Header"
           descriptions="Header Descriptions"
         ></HeaderContent>
@@ -22,13 +22,11 @@
 
           PARAMETERS
           :imgURL ( URL for about us )
-         -->
-        <AboutContent
-          imgURL="/img/about.jpg"
-        >
-          <template v-slot:about>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolorum rerum hic exercitationem odit, soluta eaque eos corporis quam alias ipsum repudiandae blanditiis tempore eligendi numquam temporibus! Molestias, quos beatae?
-          </template>
+        -->
+        <AboutContent imgURL="https://www.digiwebconsulting.com/img/about-us.png">
+          <template
+            v-slot:about
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolorum rerum hic exercitationem odit, soluta eaque eos corporis quam alias ipsum repudiandae blanditiis tempore eligendi numquam temporibus! Molestias, quos beatae?</template>
         </AboutContent>
 
         <ServiceContent>
@@ -40,48 +38,36 @@
               PARAMETERS
 
               :imgURL (image url)
-             -->
-            <Service
-              imgURL="/img/icon1.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            -->
+            <Service imgURL="/img/icon1.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon2.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon2.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon3.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon3.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
 
-            <Service
-              imgURL="/img/icon4.png"
-              title="Service Title"
-            >
-              <template v-slot:content>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?
-              </template>
+            <Service imgURL="/img/icon4.png" title="Service Title">
+              <template
+                v-slot:content
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus excepturi quisquam temporibus non nisi doloribus cumque cum molestiae odio velit ratione perspiciatis assumenda numquam quam porro, sed quasi quia possimus?</template>
             </Service>
           </template>
         </ServiceContent>
 
         <!-- 
           Portfolio
-         -->
+        -->
         <PortfolioContent>
           <template v-slot:portfolio>
             <!-- 
@@ -92,14 +78,13 @@
               :name ( Name of the Project )
               :project ( Name of Project Type)
               :projectURL ( Link Address to project)
-             -->
+            -->
             <Project
-                imgURL='/img/about.jpg'
-                name='Project Name'
-                project='Project Type'
-                projectURL='https://google.com'
-              />
-
+              imgURL="/img/about.jpg"
+              name="Project Name"
+              project="Project Type"
+              projectURL="https://google.com"
+            />
           </template>
         </PortfolioContent>
         <ContactContent
@@ -114,5 +99,10 @@
 </template>
 <script>
 export default {
-}
+  head() {
+    return {
+      title: "lowlay"
+    };
+  }
+};
 </script>
